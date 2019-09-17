74c51ce9350518f3d67afa0ede18f395f8f97e4a
Raevan <raevanparson@gmail.com>
2019-09-17 03:51:00 +0000

    Added the new pages for new location entry and the associated buttons to make them active and recordable

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  
+
   def index
     @places = Place.all
   end
@@ -14,6 +14,26 @@ class PlacesController < ApplicationController
 
   def create
     current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
     redirect_to root_path
   end
 
