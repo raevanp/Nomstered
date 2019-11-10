2507755ccfa3fc20f441b40267ac4dfe89ac4b15
Raevan <raevanparson@gmail.com>
2019-11-10 22:58:51 +0000

    Troubleshooting for photo uploads

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,16 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
-    place_photos_path(@place)
+    redirect_to place_path(@place)
   end
-
 
   private
 
   def photo_params
-    params.require(:photo).permit(:text, :picture)
+    params.require(:photo).permit(:caption, :picture)
   end
 end
 
